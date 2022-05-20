08f0b74ffb390649629e09038defc072009d660b
Yuminyumin <102581107+Yuminyumin@users.noreply.github.com>
2022-05-20 23:46:33 +0900

    Update server.js

--- src/server.js
+++ src/server.js
@@ -23,12 +23,21 @@ function onSocketClose() {
 
 wss.on("connection", (socket) => {
     sockets.push(socket);
+    socket["nickname"] = "Anon";
     console.log("Connected to Browser ✅");
     socket.on("close", onSocketClose);
     socket.on("message", onSocketMessage);
     socket.send("hello!!!");
-    socket.on("message", (message) => {
-      sockets.forEach((aSocket) => aSocket.send(message));
+    socket.on("message", (msg) => {
+      const message = JSON.parse(msg);
+      switch (message.type) {
+        case "new_message":
+          sockets.forEach((aSocket) =>
+            aSocket.send(`${socket.nickname}: ${message.payload}`)
+          );
+        case "nickname":
+          socket["nickname"] = message.payload;
+      }
     });
   });
   
