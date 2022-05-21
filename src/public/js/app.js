1704437e7d4e275ec82b1f7592bef4ea6254e3eb
Yuminyumin <102581107+Yuminyumin@users.noreply.github.com>
2022-05-21 17:23:23 +0900

    Update app.js

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -35,6 +35,7 @@ function handleSubmit(event) {
     event.preventDefault();
     const input = nickForm.querySelector("input");
     socket.send(makeMessage("nickname", input.value));
+    input.value = "";
   }
   
   messageForm.addEventListener("submit", handleSubmit);
